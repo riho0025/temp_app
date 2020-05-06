e9be4fff9d9e1a98b62b7d225024f675be0788c7
riho0025 <yuxxx26xx@gmail.com>
2020-05-06 19:29:43 +0900

    インデックス追加

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,5 @@ Rails.application.routes.draw do
   devise_for :users
   root to: "records#index"
   resources :records, only: [:index, :new, :create, :show]
-  resources :users, only: [:edit, :update, :show]
+  resources :users, only: [:edit, :update, :show, :index]
 end
